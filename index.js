733141cf1a3024ae7198c5656ba8bdc8be233816
Adish K T <161046695+adishkt@users.noreply.github.com>
2026-09-16 23:39:37 +0530

    install all files to start project

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // const heading = React.createElement("h1", { id: "heading" }, "Hello boys");
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
@@ -14,3 +16,4 @@ const parent = React.createElement("div", { id: "parent" }, [
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(parent);
+ 
