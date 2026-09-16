b90854e9fb595173de7baf6dba040cf31e8e5e51
Adish K T <161046695+adishkt@users.noreply.github.com>
2026-09-16 15:33:02 +0530

    edited file

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 // const heading = React.createElement("h1", { id: "heading" }, "Hello boys");
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
+const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "hello h1"),
     React.createElement("h2", {}, "Hello h2"),
   ]),
-);
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "hello h1"),
+    React.createElement("h2", {}, "Hello h2"),
+  ]),
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
