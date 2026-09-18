5e6116eb99afb811319c6a328964c614a7617d35
Adish K T <161046695+adishkt@users.noreply.github.com>
2026-09-18 23:39:47 +0530

    multiple state

--- reactProject/src/components/PackingList.jsx
+++ reactProject/src/components/PackingList.jsx
@@ -1,14 +1,19 @@
 import Items from "./Items";
 import { items } from "../utils/mockdata";
-import { useState } from "react";
+import { use, useState } from "react";
 
 function PackingList() {
   const [index, setIndex] = useState(0);
+  const [ShowMore, setShowMore] = useState(true);
 
   function handleClick() {
-    index==3 ? setIndex(0) : setIndex(index+1);
-    
+    index == 3 ? setIndex(0) : setIndex(index + 1);
   }
+
+  function handleShowMore() {
+    setShowMore(!ShowMore);
+  }
+
   const item = items[index];
   return (
     <>
@@ -16,8 +21,12 @@ function PackingList() {
       <h2>{item.name}</h2>
       <img src={item.url} width="200px" height="200px" />
       <br />
+      <p>{!ShowMore && item.description}</p>
 
       <button onClick={handleClick}>Next</button>
+      <button onClick={handleShowMore}>
+        {ShowMore ? "Show" : "Hide"}description
+      </button>
     </>
   );
 }
--- reactProject/src/utils/mockdata.js
+++ reactProject/src/utils/mockdata.js
@@ -3,24 +3,28 @@ export const items = [
     id: 1,
     name: "Clothes",
     isPacked: true,
-    url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtUSpgdbWN5BwnBPiPeIkqWUPuhk0xA9aRcp206Kkr2A&s=10"
+    url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtUSpgdbWN5BwnBPiPeIkqWUPuhk0xA9aRcp206Kkr2A&s=10",
+    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library in London, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets."
   },
   {
     id: 2,
     name: "Hair Dryer",
     isPacked: false,
-    url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEMlVXK4bAWv0d7a3N6KNweoQacCMGwSwngUA1rpEOMQ&s"
+    url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEMlVXK4bAWv0d7a3N6KNweoQacCMGwSwngUA1rpEOMQ&s",
+    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library in London, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets."
   },
   {
     id: 3,
     name: "PowerBank",
     isPacked: false,
-    url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJTcBTG3JcGdrIQEed8c4hu781R1peglIsw4Krortpbw&s=10"
+    url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJTcBTG3JcGdrIQEed8c4hu781R1peglIsw4Krortpbw&s=10",
+    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library in London, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets."
   },
   {
     id: 4,
     name: "conditioner",
     isPacked: true,
-    url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2_M0WSabBoPqTeJnj1oDglUNl3tYBAZ4GDtKBUwy5Sg&s=10"
+    url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2_M0WSabBoPqTeJnj1oDglUNl3tYBAZ4GDtKBUwy5Sg&s=10",
+    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library in London, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets."
   },
 ];
