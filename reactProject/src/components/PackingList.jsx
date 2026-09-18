d8c361cf700fe52e461b1700a37e04da24244a5a
Adish K T <161046695+adishkt@users.noreply.github.com>
2026-09-18 23:24:14 +0530

    states and useState

--- reactProject/src/components/PackingList.jsx
+++ reactProject/src/components/PackingList.jsx
@@ -1,50 +1,23 @@
 import Items from "./Items";
-
-const items = [
-  {
-    id: 1,
-    name: "Clothes",
-    isPacked: true,
-  },
-  {
-    id: 2,
-    name: "Hair Dryer",
-    isPacked: false,
-  },
-  {
-    id: 3,
-    name: "PowerBank",
-    isPacked: false,
-  },
-  {
-    id: 4,
-    name: "conditioner",
-    isPacked: true,
-  },
-];
+import { items } from "../utils/mockdata";
+import { useState } from "react";
 
 function PackingList() {
-  function handleClick() {
-    alert("Hey i am clicked");
-  }
+  const [index, setIndex] = useState(0);
 
+  function handleClick() {
+    index==3 ? setIndex(0) : setIndex(index+1);
+    
+  }
+  const item = items[index];
   return (
     <>
       <h1>List of items</h1>
+      <h2>{item.name}</h2>
+      <img src={item.url} width="200px" height="200px" />
+      <br />
 
-      {items.map((item) => {
-        return <Items key={item.id} data={item}></Items>;
-      })}
-
-      <button onClick={handleClick}>I am a button</button>
-      <button onClick={function handleCli(){
-        alert("hey");
-      }}>I am a button</button>
-
-      <button onClick={() => {
-        alert("hey");
-      }}>I am a button</button>
-
+      <button onClick={handleClick}>Next</button>
     </>
   );
 }
---
+++ reactProject/src/utils/mockdata.js
@@ -0,0 +1,26 @@
+export const items = [
+  {
+    id: 1,
+    name: "Clothes",
+    isPacked: true,
+    url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtUSpgdbWN5BwnBPiPeIkqWUPuhk0xA9aRcp206Kkr2A&s=10"
+  },
+  {
+    id: 2,
+    name: "Hair Dryer",
+    isPacked: false,
+    url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEMlVXK4bAWv0d7a3N6KNweoQacCMGwSwngUA1rpEOMQ&s"
+  },
+  {
+    id: 3,
+    name: "PowerBank",
+    isPacked: false,
+    url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJTcBTG3JcGdrIQEed8c4hu781R1peglIsw4Krortpbw&s=10"
+  },
+  {
+    id: 4,
+    name: "conditioner",
+    isPacked: true,
+    url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2_M0WSabBoPqTeJnj1oDglUNl3tYBAZ4GDtKBUwy5Sg&s=10"
+  },
+];
